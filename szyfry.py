d9d7d144cb57f6b14514045f28ebe462f0b3fe92
jakubsadowski08 <33435745+jakubsadowski08@users.noreply.github.com>
2017-11-13 09:26:33 +0100

    Update szyfry.py

--- szyfry.py
+++ szyfry.py
@@ -1,13 +1,13 @@
 def cezar(tekst, przesuniecie):
     alfabet ="abcdefghijklmnoprsuvz" #napisz tu dokladny alfabet :D
     koncowy_tekst =""
-    for i in tekst:
+    for i in tekst.lower():
         koncowy_tekst+=(alfabet[alfabet.index(i)+przesuniecie])
     return koncowy_tekst
 def podstawieniowy(tekst, alfabet_2):
     alfabet ="abcdefghijklmnoprsuvz" #napisz tu dokladny alfabet :D
     koncowy_tekst=""
-    for i in tekst:
+    for i in tekst.lower():
         koncowy_tekst+=alfabet_2[alfabet.index(i)]
     return koncowy_tekst
 def deZKluczem(kod, klucz):
@@ -15,10 +15,10 @@ def deZKluczem(kod, klucz):
     deszyfr =""
     tmp =0
     tab =[]
-    for i in klucz:
+    for i in klucz.lower():
         tab.append(alfabet.index(i))
-    for i in kod:
-        deszyfr+=alfabet[alfabet.index(i)-1 - tab[j]]
+    for i in kod.lower():
+        deszyfr+=alfabet[alfabet.index(i)-1 - tab[tmp]]
         tmp = tmp+1
     return deszyfr
         
