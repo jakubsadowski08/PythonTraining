8ac763fe5c42346c802e4179120ff343dc0d5a15
jakubsadowski08 <33435745+jakubsadowski08@users.noreply.github.com>
2017-11-23 20:33:17 +0100

    Add files via upload

--- snake/config.py
+++ snake/config.py
@@ -2,11 +2,15 @@ class Config:
     display_width = 800
     display_height = 600
     white = (255,255,255)
-    apple_x = 700
-    apple_y = 500
+    apple_x = display_width * 0.7
+    apple_y = display_height * 0.2
+    banana_x = display_width * 0.5
+    banana_y = display_height * 0.5
     head_x = display_width * 0.5
     head_y = display_height * 0.5
     body_x = []
     body_y = []
     directions = {"Left":0,"Up":1,"Right":2,"Down":3}
     direction =directions["Right"]
+    speed = 1
+    boost_lenght = 150
